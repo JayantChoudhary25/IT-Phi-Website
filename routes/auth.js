64884b9d261dbc6f8acb6c45bd433647056bd044
Jay <jayantchoudhary271@gmail.com>
2023-12-17 19:04:00 +0530

    Code fix

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../utils/uploadImage");
+
 const {
   register,
   login,
@@ -27,19 +27,5 @@ router.post("/updatePassword", isAuthenticatedUser, updatePassword);
 router.route("/forgotpassword").post(forgotPassword);
 router.route("/resetpassword/:resetToken").put(resetPassword);
 
-// Uploading images
-router.post("/upload", upload.array("images", 5), (req, res) => {
-  // Check if files were uploaded successfully
-  if (!req.files || req.files.length === 0) {
-    return res.status(400).json({ error: "No files uploaded." });
-  }
-
-  // Get the uploaded image URLs from Cloudinary
-  const imageUrls = req.files.map((file) => file.path);
-
-  // You can save these image URLs to your product model or perform other actions as needed
-
-  res.status(200).json({ imageUrls });
-});
 
 module.exports = router;
